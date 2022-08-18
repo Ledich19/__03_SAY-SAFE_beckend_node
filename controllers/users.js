55c2f6a8a890c0d0df50e7d04cd53efab5d793e2
Aleksandr <ledich19@gmail.com>
2022-08-18 21:47:25 +0300

    add nodemailer

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
-
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-const { userExtractor } = require('../utils/middleware')
+const {
+  userExtractor
+} = require('../utils/middleware')
+const nodemailer = require('nodemailer')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
@@ -16,11 +18,32 @@ usersRouter.get('/', async (request, response) => {
 })
 
 // get my info
-usersRouter.get('/me',userExtractor , async (request, response) => {
-  console.log('111111111111111111')
+usersRouter.get('/me', userExtractor, async (request, response) => {
+
   const userId = request.user.id
   const user = await User.findById(userId)
   response.json(user)
+})
+
+//verify new user
+usersRouter.get('/verify', async (request, response) => {
+  const hash = request.query.hash
+  const decodedToken = await jwt.verify(hash, process.env.SECRET)
+  const {
+    username,
+    email,
+    passwordHash
+  } = decodedToken
+  const user = new User({
+    username,
+    email,
+    passwordHash
+  })
+
+  console.log('\x1b[33m user', user, '\x1b[0m')
+  const savedUser = await user.save()
+  response.status(200).json(savedUser)
+
 })
 
 // get user by id
@@ -28,26 +51,28 @@ usersRouter.get('/:id', async (request, response) => {
   const id = request.params.id
   const user = await User.findById(id)
   const userResponse = {
-    username: user.username ,
-    email: user.email ,
-    avatar: user.avatar ,
-    lastSeen: user.lastSeen ,
-    name: user.name ,
-    lastName: user.lastName ,
-    age: user.age ,
-    Country: user.Country ,
-    city: user.city ,
-    education: user.education ,
-    gender: user.gender ,
-    aboutMe: user.aboutMe ,
-    myFavoriteThem: user.myFavoriteThem ,
-    Photos: user.Photos ,
+    username: user.username,
+    email: user.email,
+    avatar: user.avatar,
+    lastSeen: user.lastSeen,
+    name: user.name,
+    lastName: user.lastName,
+    age: user.age,
+    Country: user.Country,
+    city: user.city,
+    education: user.education,
+    gender: user.gender,
+    aboutMe: user.aboutMe,
+    myFavoriteThem: user.myFavoriteThem,
+    Photos: user.Photos,
   }
   response.json(userResponse)
 })
 
 //new user
 usersRouter.post('/registration', async (request, response) => {
+  console.log('1111')
+
   const {
     username,
     email,
@@ -74,14 +99,49 @@ usersRouter.post('/registration', async (request, response) => {
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
-  const user = new User({
+
+  const token = jwt.sign({
     username,
     email,
     passwordHash
+  },
+  process.env.SECRET, {
+    expiresIn: '1d'
+  }
+  )
+
+  const hesh =  `${request.protocol}://${request.get('host')}/verify?hash=${token}`
+  console.log('hesh', hesh)
+
+  let transporter = await nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'SaySafeApp@outlook.com', // generated ethereal user
+      pass: '190289ale' // generated ethereal password
+    },
+    tls: {
+      ciphers:'SSLv3'
+    }
   })
-  console.log('\x1b[33m user', user, '\x1b[0m')
-  const savedUser = await user.save()
-  response.status(201).json(savedUser)
+
+  // send mail with defined transport object
+  await transporter.sendMail({
+    from: '"Alexander Chumachenko" <SaySafeApp@outlook.com>', // sender address
+    to: 'ledich19@gmail.com', // list of receivers
+    subject: 'hesh', // Subject line
+    text: hesh, // plain text body
+  })
+
+  //console.log('Message sent: %s', info.messageId)
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  response.status(200).send('check uour email')
 })
 
 
